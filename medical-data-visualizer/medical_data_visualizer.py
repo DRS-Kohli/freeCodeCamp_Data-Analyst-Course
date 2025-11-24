8f496a77ee1b55c541b948c5ab6bf1c81f7aef05
DRS-Kohli <divyarehatsingh.k@gmail.com>
2025-11-24 15:58:45 +0530

    Add 3rd Project

---
+++ medical-data-visualizer/medical_data_visualizer.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = pd.read_csv('medical_examination.csv')
+
+# 2
+df['overweight'] = (df['weight'] / ((df['height']/100)**2) > 25).astype(int)
+print(df.head())
+
+# 3
+def normalize(col):
+    return(col > 1).astype(int)
+df['cholesterol'] = normalize(df['cholesterol'])
+df['gluc'] = normalize(df['gluc'])
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = pd.melt(
+        df,
+        id_vars=['cardio'],
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
+
+
+    # 6
+    df_cat = (
+        df_cat
+        .groupby(['cardio', 'variable', 'value'])
+        .size()
+        .reset_index(name='total')   # rename the count column to 'total' for plotting
+    )
+    
+
+    # 7
+    catplot = sns.catplot(
+        data=df_cat,
+        kind='bar',
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio'
+    )
+
+
+    # 8
+    fig = catplot.fig
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = df.copy()
+    df_heat = df_heat[
+        (df_heat['ap_lo'] <= df_heat['ap_hi']) &
+        (df_heat['height'] >= df_heat['height'].quantile(0.025)) &
+        (df_heat['height'] <= df_heat['height'].quantile(0.975)) &
+        (df_heat['weight'] >= df_heat['weight'].quantile(0.025)) &
+        (df_heat['weight'] <= df_heat['weight'].quantile(0.975))
+    ]
+     
+    # 12
+    corr = df_heat.corr()
+
+    # 13
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+
+
+    # 14
+    fig, ax = plt.subplots(figsize=(12, 10))
+
+    # 15
+    sns.heatmap(
+        corr,
+        mask=mask,
+        annot=True,
+        fmt='.1f',
+        center=0,
+        vmax=0.3,
+        vmin=-0.1,
+        square=True,
+        linewidths=0.5,
+        cbar_kws={'shrink': 0.5},
+        ax=ax
+    )
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
